9c96f06e21e5f1b356a66bc1508a75814fa3bea7
hpauzer <heatherpauzer@Heathers-MBP-2.attlocal.net>
2017-10-13 20:32:46 -0500

    coding flashcards

--- BasicCard.js
+++ BasicCard.js
@@ -17,4 +17,12 @@ var flashcard = function() {
 
 flashcard();
 
+var card1 = new BasicCard("What is a Quarter-Pounder with cheese called in Paris?",
+    "back", "A Royale with Cheese");
+
+var card2 = new BasicCard("Who said, 'There's no crying in baseball?'",
+    "Tom Hank's character, Jimmy, in A League of Their Own.");
+
+var card3 = new BasicCard("In Billy Madison, who rules?", "O'Doyle Rules");
+
 module.exports = { BasicCard };
